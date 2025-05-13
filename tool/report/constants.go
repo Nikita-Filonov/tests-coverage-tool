0162f669e1ae09977d3cf03163455fe8d5ade9d3
Nikita Filonov <nikita.filonov@dif.tech>
2025-05-13 15:48:55 +0300

    Update constants.go

--- tool/report/constants.go
+++ tool/report/constants.go
@@ -1,7 +1,5 @@
 package report
 
-type StateKey string
-
 const (
 	sourceReportFile      = "./submodules/tests-coverage-report/build/index.html"
 	destinationReportFile = "./tool/report/templates/index.html"
